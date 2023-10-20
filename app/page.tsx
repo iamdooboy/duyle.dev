ee8a0459ceca8ebdbe4e9d3aa4a5adcc5607a333
Duy Le <l.duy194@gmail.com>
2023-10-19 22:40:01 -0400

    added spotlight component

--- app/page.tsx
+++ app/page.tsx
@@ -1,22 +1,29 @@
+import type { ComponentPropsWithoutRef } from 'react'
+import { CardSpotlight } from '@/components/card-spotlight'
 
-import type { ComponentPropsWithoutRef } from 'react';
-
-interface Props extends ComponentPropsWithoutRef<"a"> {
-  label: string;
+interface Props extends ComponentPropsWithoutRef<'a'> {
+	label: string
 }
 
 function Badge(props: Props) {
-  return (
-    <a
-      {...props}
-      target="_blank"
-      className="border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline"
-    />
-  );
+	return (
+		<a
+			{...props}
+			target='_blank'
+			className='border border-neutral-200 dark:border-neutral-700 bg-neutral-50 dark:bg-neutral-800 rounded p-1 text-sm inline-flex items-center leading-4 text-neutral-900 dark:text-neutral-100 no-underline'
+		/>
+	)
 }
 
 export default function Home() {
-  return (
-    <div>hello world</div>
-  )
+	return (
+		<div className='space-y-2'>
+			<p className='leading-7 [&:not(:first-child)]:mt-6'>
+				Crafting UIs with React since 2016. Passionate about interface design
+				and attention to detail, striving to create great experiences.
+			</p>
+			<div className='text-lg font-semibold'>Projects</div>
+			<CardSpotlight />
+		</div>
+	)
 }
---
+++ components/card-spotlight.tsx
@@ -0,0 +1,137 @@
+'use client'
+import React, { useRef, useState } from 'react'
+
+import {
+	ChevronDownIcon,
+	CircleIcon,
+	PlusIcon,
+	StarIcon
+} from '@radix-ui/react-icons'
+
+import { Button } from '@/ui/button'
+import {
+	Card,
+	CardContent,
+	CardDescription,
+	CardHeader,
+	CardTitle
+} from '@/ui/card'
+
+import {
+	DropdownMenu,
+	DropdownMenuCheckboxItem,
+	DropdownMenuContent,
+	DropdownMenuItem,
+	DropdownMenuLabel,
+	DropdownMenuSeparator,
+	DropdownMenuTrigger
+} from '@/ui/dropdown-menu'
+import { Separator } from '@/ui/separator'
+
+export function CardSpotlight() {
+	const divRef = useRef<HTMLDivElement>(null)
+	const [isFocused, setIsFocused] = useState(false)
+	const [position, setPosition] = useState({ x: 0, y: 0 })
+	const [opacity, setOpacity] = useState(0)
+
+	const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+		if (!divRef.current || isFocused) return
+
+		const div = divRef.current
+		const rect = div.getBoundingClientRect()
+
+		setPosition({ x: e.clientX - rect.left, y: e.clientY - rect.top })
+	}
+
+	const handleFocus = () => {
+		setIsFocused(true)
+		setOpacity(1)
+	}
+
+	const handleBlur = () => {
+		setIsFocused(false)
+		setOpacity(0)
+	}
+
+	const handleMouseEnter = () => {
+		setOpacity(1)
+	}
+
+	const handleMouseLeave = () => {
+		setOpacity(0)
+	}
+
+	return (
+		<Card
+			ref={divRef}
+			onMouseMove={handleMouseMove}
+			onFocus={handleFocus}
+			onBlur={handleBlur}
+			onMouseEnter={handleMouseEnter}
+			onMouseLeave={handleMouseLeave}
+			className='overflow-hidden relative bg-gradient-to-r from-black to-slate-950 shadow-2xl'
+		>
+			<div
+				className='pointer-events-none absolute -inset-px opacity-0 transition duration-300'
+				style={{
+					opacity,
+					background: `radial-gradient(600px circle at ${position.x}px ${position.y}px, rgba(255,182,255,.1), transparent 40%)`
+				}}
+			/>
+			<CardHeader className='grid grid-cols-[1fr_110px] items-start gap-4 space-y-0'>
+				<div className='space-y-1'>
+					<CardTitle>Project 1</CardTitle>
+					<CardDescription>
+						Beautifully designed components built with Radix UI and Tailwind
+						CSS.
+					</CardDescription>
+				</div>
+				<div className='flex items-center space-x-1 rounded-md bg-secondary text-secondary-foreground'>
+					<Button variant='secondary' className='px-3 shadow-none'>
+						<StarIcon className='mr-2 h-4 w-4' />
+						Star
+					</Button>
+					<Separator orientation='vertical' className='h-[20px]' />
+					<DropdownMenu>
+						<DropdownMenuTrigger asChild>
+							<Button variant='secondary' className='px-2 shadow-none'>
+								<ChevronDownIcon className='h-4 w-4 text-secondary-foreground' />
+							</Button>
+						</DropdownMenuTrigger>
+						<DropdownMenuContent
+							align='end'
+							alignOffset={-5}
+							className='w-[200px]'
+							forceMount
+						>
+							<DropdownMenuLabel>Suggested Lists</DropdownMenuLabel>
+							<DropdownMenuSeparator />
+							<DropdownMenuCheckboxItem checked>
+								Future Ideas
+							</DropdownMenuCheckboxItem>
+							<DropdownMenuCheckboxItem>My Stack</DropdownMenuCheckboxItem>
+							<DropdownMenuCheckboxItem>Inspiration</DropdownMenuCheckboxItem>
+							<DropdownMenuSeparator />
+							<DropdownMenuItem>
+								<PlusIcon className='mr-2 h-4 w-4' /> Create List
+							</DropdownMenuItem>
+						</DropdownMenuContent>
+					</DropdownMenu>
+				</div>
+			</CardHeader>
+			<CardContent>
+				<div className='flex space-x-4 text-sm text-muted-foreground'>
+					<div className='flex items-center'>
+						<CircleIcon className='mr-1 h-3 w-3 fill-sky-400 text-sky-400' />
+						TypeScript
+					</div>
+					<div className='flex items-center'>
+						<StarIcon className='mr-1 h-3 w-3' />
+						20k
+					</div>
+					<div>Updated April 2023</div>
+				</div>
+			</CardContent>
+		</Card>
+	)
+}
