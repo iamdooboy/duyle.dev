8fcfb13ad92dfcd5db92ba989e95d1acca1fe877
Duy Le <l.duy194@gmail.com>
2024-09-06 13:46:44 -0500

    particle effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,31 @@
 import { BlogPosts } from "./_components/blog-posts"
+import { Particles } from "./_components/particles"
 export default function Page() {
   return (
-    <section>
-      <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
-        My Portfolio
-      </h1>
-      <p className="mb-4">
-        {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
+    <>
+      <section>
+        <h1 className="mb-8 text-2xl font-semibold tracking-tighter">
+          My Portfolio
+        </h1>
+        <p className="mb-4">
+          {`I'm a Vim enthusiast and tab advocate, finding unmatched efficiency in
         Vim's keystroke commands and tabs' flexibility for personal viewing
         preferences. This extends to my support for static typing, where its
         early error detection ensures cleaner code, and my preference for dark
         mode, which eases long coding sessions by reducing eye strain.`}
-      </p>
-      <div className="my-8">
-        <BlogPosts />
-      </div>
-    </section>
+        </p>
+        <div className="my-8">
+          <BlogPosts />
+        </div>
+      </section>
+      <Particles
+        className="absolute inset-0 -z-10"
+        quantity={50}
+        ease={70}
+        size={0.05}
+        staticity={40}
+        color="#ffffff"
+      />
+    </>
   )
 }
